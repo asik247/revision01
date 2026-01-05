a80fef17e8ad07c4eede0457e6bb214c15a12c54
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-06 00:06:42 +0600

    ternay operator explore

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function sum (a,b,c){
 let taka = [10,20,30];
 let result = (sum(...taka))
 console.log(result);
-
-
-
-
 let basket1 = ["apple", "banana"];
 let basket2 = ["mango", "orange"];
 console.log(basket1 + basket2);
 let allFruits = [...basket1, ...basket2];
 console.log(allFruits);
+// Ternary Operator:-
+let age = 15
+let text = (age < 18)?"Minor":"Adult"
+console.log(text);
